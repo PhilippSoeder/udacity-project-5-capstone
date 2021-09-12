6b104451067ba21bc7faaa2581c03e89f11c9c1f
philippsoder <philippsoeder@gmx.de>
2021-09-12 17:09:10 +0200

    Add security-check job to pipeline

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def index():
     return "Hello! This is Philipp Söders capstone project for the Udacity AWS Cloud DevOps Engineer Nanodegree Program."
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='127.0.0.1', port=80)

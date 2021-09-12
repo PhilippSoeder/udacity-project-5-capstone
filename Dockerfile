7ae6a368125d07ddd917582f24d46b899c37e830
philippsoder <philippsoeder@gmx.de>
2021-09-12 17:21:01 +0200

    Update app

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,4 @@ COPY . app.py /application/
 RUN pip install --no-cache-dir --trusted-host pypi.python.org --requirement requirements.txt
 EXPOSE 80
 CMD ["python", "app.py"]
+CMD ["python", "-m" , "flask", "run", "--host=0.0.0.0", "--port=80"]
--- app.py
+++ app.py
@@ -4,6 +4,3 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello! This is Philipp Söders capstone project for the Udacity AWS Cloud DevOps Engineer Nanodegree Program."
-
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=80)

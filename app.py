e0f5e97d86aaaf4f22090a6d812cd6da7fc4f812
philippsoder <philippsoeder@gmx.de>
2021-09-12 13:16:48 +0200

    Test updating app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "Hello! This is Philipp Söders capstone project for the Udacity AWS Cloud DevOps Engineer Nanodegree Program."
+    return "Hello! This is Philipp Söders capstone project for the Udacity AWS Cloud DevOps Engineer Nanodegree Program. Updated app."
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
